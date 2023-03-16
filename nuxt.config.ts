18e7c380742460c816bbf7b8258fa25325223289
razmatinyan <matinyanraz@mail.ru>
2023-03-16 22:15:03 +0400

    Added viewport metatag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,12 @@ export default defineNuxtConfig({
           'utils/**'
         ]
     },
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'user-scalable=0, initial-scale=1.0, maximum-scale=1.0, width=device-width',
+        }
+    },
     css: ["~/assets/reset.css", "~/assets/fonts.css", "~/assets/main.css"],
     alias: {
         assets: "/<rootDir>/assets"
